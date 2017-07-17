965e9a9e833b03daf06a8146c4ca244971e71955
pmyakishev <contactfaze@gmail.com>
2017-07-17 11:52:01 -0400

    fixed popup

--- Bestbuy/src/main/java/ui/WhenYouWantOrder.java
+++ Bestbuy/src/main/java/ui/WhenYouWantOrder.java
@@ -19,7 +19,7 @@ public class WhenYouWantOrder extends BBCommonAPI {
 
     public  WhenYouWantOrder confirmPhone () throws InterruptedException{
         sleepFor(2);
-        userPhone.sendKeys(notMyPhone);
+        userPhone.sendKeys("555-5555");
         continueToPaymentInfo.click(); sleepFor(5);
         return new WhenYouWantOrder();
     }
